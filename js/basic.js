ae6190541cd8a818011fbd75d9a8e1c01abda4f7
Lino Espinoza <lino.espinoza@entel.pe>
2017-11-15 18:19:23 -0500

    Fix js

---
+++ js/basic.js
@@ -0,0 +1,17 @@
+
+// jQuery(document).ready()
+$(document).ready(function(){
+	
+	// Obteniendo el valor del atributo class del elemento cuyo id sea role
+	var role = $(".custom-select").append('<option value="teacher">Teacher</option>');
+
+	// Imprimiendo en consola
+	//console.log(role);
+
+	// DOM Manipulation
+	console.log($("#name").attr("value"));
+	
+	$(".custom-select").attr("class", "claseDefault");
+
+	// @To Do (revisar manipulación de data attributes)
+});
---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Esto es un comentario
+
+console.log("Esto es una prueba");
+
+// String, cadena o texto
+// 'Fruta'
+// "Fruta del campo"
+
+// Numbers, números
+// 1
+// 2
+// 3.5
+// 3.99999999
+// 1.0
+
+// Operador suma (+)
+console.log(1 + 3);
+
+// Operador resta (-)
+console.log(100 - 37);
+
+// Operador multiplicación (*)
+console.log(100 * 1);
+
+// Operador division (/)
+console.log(30 / 3);
+
+var campoDeTexto = true;
+console.log(campoDeTexto);
+
+
