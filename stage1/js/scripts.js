c37d593b88537b340ee30e044e146effb6b317eb
Lino Espinoza <lino.espinoza@entel.pe>
2017-12-06 18:13:28 -0500

    Se agregan las clases de la sesion13

--- stage1/js/scripts.js
+++ stage1/js/scripts.js
@@ -26,7 +26,54 @@ $(document).ready(function(){
 	// Método attr: Reemplaza el valor de un atributo específico
 	// ('input#name').attr('placeholder', 'Enter your name');
 
-	$('#registration-form').submit(function(event){
-		console.log(event);
+	$("ul.dias > li").click(function(event){
+		// console.log('click', $(this).text());
 	});
+
+	$("ul.dias > li").dblclick(function(event){
+		// console.log('click', $(this).text());
+	});
+
+	$("ul.dias > li").on('click mouseover', function(event){
+		// console.log('clicked', $(this).text());
+	})
+
+	$(".formulario > input").focus(function(event){
+		// console.log('Focus', $(this).val());
+	})
+
+	$(".formulario > input").blur(function(event){
+		// console.log('Blur', $(this).val());
+	})
+
+	//$("ul.dias").show();
+
+
+	jQuery.fx.speeds.tortuga = 4000;
+	jQuery.fx.speeds.veloz = 100;
+
+	// $("ul.dias").fadeIn("veloz");
+
+	$("#hideDias").click(function(){
+		$("ul.dias").hide();
+	})
+
+
+	$("#fadeOutDias").click(function(){
+		$("ul.dias").fadeOut("tortuga");
+	})
+
+	$("ul.dias").slideDown('tortuga');
+
+	$("#slideUpDias").click(function(){
+		$("ul.dias").slideUp("tortuga", function(){
+			$("#slideUpDias").remove();	
+		});
+	})
+
+
+
+
+
+
 })
